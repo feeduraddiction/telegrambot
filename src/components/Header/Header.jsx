be2d54468caa8505fc3716b64b937f425eb4a55b
Ilya Charniatsou <alfanex62@gmail.com>
2024-05-28 13:50:55 +0400

    user

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,17 @@ export const Header = () => {
   return (
     <div className="header">
       <Button onClick={onClose}>Закрыть</Button>
-      <span className="username">{user?.username}</span>
+      <div className="user">
+        <div className="img">
+          <img src={user?.photo_url} alt="user-photo" />
+        </div>
+        <div className="name">
+          <span className="username">{user?.username || "username"}</span>
+          <span className="name">
+            {user?.first_name || "John"} {user?.last_name || "Doe"}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
